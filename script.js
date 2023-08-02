159446d6fea308586fbdb489ddd11443e5e33316
Prueba1-Jorge <jorgeprueba1@gmai.com>
2023-08-02 10:59:11 -0500

    tercer  Commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function sumar(num1, num2) {
     return num1 + num2;
   }
   
-  // Ejemplo de uso
+  // Ejemplo de uso, commmit 2
   let resultado = sumar(5, 7);
   console.log(resultado); // Output: 12
   
